360488088948e8aed1a6173d26308f8879165626
Danni <drbaxter821@gmail.com>
2016-11-13 09:47:12 -0500

    first commit

---
+++ browser/app.js
@@ -0,0 +1,19 @@
+var socket = io(window.location.origin);
+console.log(window.location.origin)
+
+whiteboard.on('draw', function(start, end, color){
+	console.log(start, end, color);
+	socket.emit('draw',start,end,color)
+})
+
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!');
+});
+
+socket.on('disconnect', function () {
+    socket.emit('user disconnected');
+  });
+
+socket.on('broadcast_event', function(start,end,color){
+	window.whiteboard.draw(start,end,color)
+})
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var socketio = require('socket.io');
 
 var http = require('http');
 var server = http.createServer();
@@ -8,12 +9,34 @@ var app = express();
 
 server.on('request', app);
 
-server.listen(1337, function () {
-    console.log('The server is listening on port 1337!');
-});
+var io = socketio(server);
+
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+io.on('connection', function (socket) {
+
+    console.log('A new client has connected!');
+    console.log(socket.id);
+
+    socket.on('disconnect', function(){
+    	console.log('you have been disconnected');
+        console.log(socket.id);
+    })
+
+    socket.on('draw',function(start,end,color){
+      socket.broadcast.emit('broadcast_event',start,end,color)
+    })
+
+});
+
+
+
+server.listen(1337, function () {
+    console.log('The server is listening on port 1337!');
 });
